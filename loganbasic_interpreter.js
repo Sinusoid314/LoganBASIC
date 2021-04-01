6e7075aa41e219b5590cf87773c4efdd8ac623bf
CheesusChice <sinusoid314@gmail.com>
2021-03-31 22:37:43 -0400

    Add files via upload
    - Created the HTML, CSS, and JavaScript skeletons.

---
+++ loganbasic_interpreter.js
@@ -0,0 +1,6 @@
+class Interpreter
+{
+  constructor()
+  {
+  }
+}
---
+++ loganbasic_main.js
@@ -0,0 +1,21 @@
+var sourceStr = "";
+var tokenList = [];
+var lexer = new Lexer();
+var interpreter = new Interpreter();
+var runBtn = document.getElementById("runBtn");
+var stopBtn = document.getElementById("stopBtn");
+
+runBtn.addEventListener("click", runBtn_OnClick);
+stopBtn.addEventListener("click", stopBtn_OnClick);
+
+function runBtn_OnClick(eventObj)
+{
+    runBtn.disabled = true;
+    stopBtn.disabled = false;
+}
+
+function stopBtn_OnClick(eventObj)
+{
+    runBtn.disabled = false;
+    stopBtn.disabled = true;
+}
