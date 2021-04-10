7ca276e4f95faec176a7728ae3f4de0249b554f2
CheesusChice <sinusoid314@gmail.com>
2021-04-09 23:50:08 -0400

    Add files via upload
    - Implemented evalExpression(), termExpr(), factorExpr(), unaryExpr(),
      primaryExpr(), and evalOperation() in Interpreter class.

--- loganbasic_interpreter.js
+++ loganbasic_interpreter.js
@@ -10,7 +10,140 @@ class Interpreter
   run()
   //
   {
+	try
+	{
 
+    }
+    catch(errorObj)
+    {
+      this.errorMsg = "Runtime error on line " + this.peekToken().lineNum + ": " + errorObj.message;
+    }
+  }
+
+  runStatement()
+  //
+  {
+
+  }
+
+  printStmt
+  //
+  {
+
+  }
+
+  assignmentStmt()
+  //
+  {
+
+  }
+
+  evalExpression()
+  //
+  {
+    return termExpr();
+  }
+
+  termExpr()
+  //
+  {
+    var firstVal, secondVal, operatorToken;
+
+    firstVal = this.factorExpr();
+
+    while(this.matchTokenTypes([TOKEN_MINUS, TOKEN_PLUS]))
+    {
+      operatorToken = this.prevToken();
+      secondVal = this.factorExpr();
+      firstVal = this.evalOperation(operatorToken, firstVal, secondVal);
+    }
+
+    return firstVal;
+  }
+
+  factorExpr()
+  //
+  {
+    var firstVal, secondVal, operatorToken;
+
+    firstVal = this.unaryExpr();
+
+    while(this.matchTokenTypes([TOKEN_SLASH, TOKEN_STAR]))
+    {
+      operatorToken = this.prevToken();
+      secondVal = this.unaryExpr();
+      firstVal = this.evalOperation(operatorToken, firstVal, secondVal);
+    }
+
+    return firstVal;
+  }
+
+  unaryExpr()
+  //
+  {
+    var val, operatorToken;
+
+    if(this.matchTokenTypes([TOKEN_MINUS]))
+    {
+      operatorToken = this.prevToken();
+      val = this.unaryExpr();
+      return this.evalOperation(operatorToken, val);
+    }
+
+    return this.primaryExpr();
+  }
+
+  primaryExpr()
+  //
+  {
+    var val;
+
+    //Literals
+    if(this.matchTokenTypes([TOKEN_STRING_LIT, TOKEN_NUMBER_LIT]))
+    {
+      return this.prevToken().literalVal;
+    }
+
+    //Nested expression
+    if(this.matchTokenTypes([TOKEN_LEFT_PAREN]))
+    {
+      val = this.evalExpression();
+      if(this.checkTokenType(TOKEN_RIGHT_PAREN))
+      {
+		this.consumeToken();
+        return val;
+      }
+      else
+      {
+        throw {message: "Expected ')' after expression."};
+      }
+    }
+  }
+
+  evalOperation(operatorToken, firstVal, secondVal)
+  //
+  {
+    switch(operatorToken)
+    {
+      case TOKEN_MINUS:
+        if(secondVal == undefined)
+        {
+          return -firstVal;
+        }
+        else
+        {
+          return firstVal - secondVal;
+        }
+
+      case TOKEN_PLUS:
+        return firstVal + secondVal;
+
+      case TOKEN_SLASH:
+        return firstVal / secondVal;
+
+      case TOKEN_STAR:
+        return firstVal * secondVal;
+    }
   }
 
   consumeToken()
