77e0bbd7be381067f6097c19d51c378f3f553c99
CheesusChice <sinusoid314@gmail.com>
2022-05-10 16:08:27 -0400

    Delete lbcore.js

--- source/core/lbcore.js
+++
@@ -1,6 +0,0 @@
-importScripts('core/object.js',
-              'core/token.js',
-              'core/bytecode.js',
-              'core/scanner.js',
-              'core/compiler.js',
-              'core/runtime.js');
