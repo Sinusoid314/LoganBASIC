b7b1982bbe2bc74d24ce581b146e038e810fa467
CheesusChice <sinusoid314@gmail.com>
2023-01-01 18:36:55 -0800

    Add files via upload

---
+++ docs/help/reference_doc.js
@@ -0,0 +1,13 @@
+var usageOpen = document.getElementById("usageOpen");
+var usageCode = document.getElementById("usageCode");
+
+usageOpen.addEventListener("click", usageOpen_onClick);
+
+function usageOpen_onClick(event)
+//
+{
+  var fileText = usageCode.innerText;
+
+  window.localStorage.setItem("fileText", fileText);
+  window.open("/source/loganbasic.html?open=local", "_blank");
+}
