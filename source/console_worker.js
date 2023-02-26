dcc5e84519a81132ba005b29cfdb759394fb1c37
CheesusChice <sinusoid314@gmail.com>
2023-02-25 20:43:02 -0800

    Add files via upload
    - Removed showEditor() and hideEditor() native funcitons.

--- source/console_worker.js
+++ source/console_worker.js
@@ -1,6 +1,4 @@
 const consoleNativeFuncs = [
-                  new ObjNativeFunc("showEditor", 0, 0, funcShowEditor),
-                  new ObjNativeFunc("hideEditor", 0, 0, funcHideEditor),
                   new ObjNativeFunc("showConsole", 0, 0, funcShowConsole),
                   new ObjNativeFunc("hideConsole", 0, 0, funcHideConsole),
                   new ObjNativeFunc("input", 1, 1, funcInput)
@@ -31,20 +29,6 @@ function onVMPrint(vm, printVal, replaceAll)
     postMessage({msgId: MSGID_CLEAR_CONSOLE});
   else
     postMessage({msgId: MSGID_PRINT, msgData: {printVal: printVal}});
-}
-
-function funcShowEditor(vm, args)
-//Tell the UI thread to show the program editor pane
-{
-  postMessage({msgId: MSGID_SHOW_EDITOR});
-  return null;
-}
-
-function funcHideEditor(vm, args)
-//Tell the UI thread to hide the program editor pane
-{
-  postMessage({msgId: MSGID_HIDE_EDITOR});
-  return null;
 }
 
 function funcShowConsole(vm, args)
--- source/core/std_funcs.js
+++ source/core/std_funcs.js
@@ -1,4 +1,4 @@
-const lbVersion = "2.0.0.100";
+const lbVersion = "2.0.0.101";
 
 const stdNativeFuncs = [
                   new ObjNativeFunc("rnd", 0, 0, funcRnd),
