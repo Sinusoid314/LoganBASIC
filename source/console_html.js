d26c5555b0f5e6d01f7c95777d16d0cea3c3ddb9
CheesusChice <sinusoid314@gmail.com>
2023-08-04 21:35:41 -0400

    Add files via upload
    - Renamed switchMode() to switchEditorMode() and moved it from main_ui module to editor_ui module.
    - Removed loadHTML() function from main_ui module.
    - Added initUI() function to main_ui module to be called from window_onLoad().
    - Moved all loadCSS() and loadScript() calls from each UI module to main_ui module.
    - Modified loadScript() in main_ui module to load the script synchronously.
    - Modified loadHTML() in main_ui module to take in an HTML string instead of a URL.
    - Replaced each component's HTML file with a JavaScript file that loads the component's HTML.

---
+++ source/console_html.js
@@ -0,0 +1,13 @@
+const consoleHTML = 
+`
+<div id="consoleDiv">
+  <label id="consoleToggle" class="toggle-open">Console</label>
+  <div id="consolePane" class="pane-open">
+    <textarea id="consoleOutput" cols="80" rows="15" wrap="off" spellcheck="false" readonly></textarea>
+    <div id="consoleInputDiv">
+      <input id="consoleInput" type="text">
+      <button id="consoleInputBtn">Enter</button>
+    </div>
+  </div>
+</div>
+`;
---
+++ source/main_ui.js
@@ -0,0 +1,185 @@
+const statusBarHTML = `<div id="statusBar">Ready.</div>`;
+const versionHTML = `<div id="version">Version 2.0.2.13 - <a href="../updates.html" target="_blank">Updates</a></div>`;
+
+var isRunning = false;
+var mainDiv = document.getElementById("mainDiv");
+var statusBar;
+var progWorker = new Worker('main_worker.js');
+var uiMessageMap = new Map();
+var paramFileURL = "";
+
+window.addEventListener("load", window_onLoad);
+
+loadCSS("debug.css");
+loadScript("debug_html.js");
+loadScript("debug_common.js");
+loadScript("debug_ui.js");
+
+loadCSS("editor.css");
+loadScript("editor_html.js");
+loadScript("editor_ui.js");
+
+loadCSS("console.css");
+loadScript("console_html.js");
+loadScript("console_common.js");
+loadScript("console_ui.js");
+
+loadCSS("canvas.css");
+loadScript("canvas_html.js");
+loadScript("canvas_common.js");
+loadScript("canvas_ui.js");
+
+loadScript("sound_common.js");
+loadScript("sound_ui.js");
+
+loadScript("sprite_common.js");
+loadScript("sprite_ui.js");
+
+
+function loadCSS(fileURL)
+//
+{
+  var link = document.createElement('link');
+
+  link.rel = "stylesheet";
+  link.type = "text/css";
+  link.href = fileURL;
+  document.head.appendChild(link);
+}
+
+function loadScript(fileURL)
+//
+{
+  var script = document.createElement('script');
+
+  script.type = "text/javascript";
+  script.async = false;
+  script.src = fileURL;
+  document.head.appendChild(script);
+}
+
+function initUI()
+//
+{
+  var urlParams = new URLSearchParams(window.location.search);
+  var toggles;
+
+  progWorker.onmessage = progUI_onMessage;
+
+  uiMessageMap.set(MSGID_PROG_DONE_SUCCESS, onMsgProgDoneSuccess);
+  uiMessageMap.set(MSGID_PROG_DONE_ERROR, onMsgProgDoneError);
+  uiMessageMap.set(MSGID_STATUS_CHANGE, onMsgStatusChange);
+  
+  if(!urlParams.has("open"))
+    paramFileURL = urlParams.get("open");
+
+  initDebugUI(mainDiv, "beforebegin");
+  initEditorUI(mainDiv, "beforeend", paramFileURL);
+  mainDiv.insertAdjacentHTML("beforeend", statusBarHTML);
+  initConsoleUI(mainDiv, "beforeend");
+  initCanvasUI(mainDiv, "beforeend");
+  mainDiv.insertAdjacentHTML("beforeend", versionHTML);
+  initSoundUI();
+  initSpriteUI();
+
+  statusBar = document.getElementById("statusBar");
+
+  toggles = document.getElementsByClassName("toggle-open");
+  for(var i = 0; i < toggles.length; i++)
+    toggles[i].addEventListener("click", toggle_onClick);
+}
+
+function cleanupUI(exitStatus)
+//Set the UI to reflect that the program has stopped running
+{
+  if(!isRunning)
+    return;
+
+  closeConsoleInput();
+  cleanupCanvas();
+  cleanupSounds();
+  cleanupSpriteSheets();
+  debugChangeUIStatus(DEBUG_UI_STATUS_DISABLED);
+  switchEditorMode();
+  
+  statusBar.innerHTML = exitStatus;
+
+  isRunning = false;
+}
+
+function startProg(source)
+//Reset the UI and signal the worker thread to start the program
+{
+  if(isRunning)
+    return;
+
+  clearConsoleOutput();
+  resetCanvas();
+  debugClearDisplays();
+  switchEditorMode();
+  
+  progWorker.postMessage({msgId: MSGID_START_PROG, msgData: {source: source}});
+
+  isRunning = true;
+}
+
+function stopProg()
+//Terminate and restart the worker thread
+{
+  progWorker.terminate();
+  progWorker = new Worker('main_worker.js');
+  progWorker.onmessage = progUI_onMessage;
+
+  //Reload saved breakpoints into the worker thread
+  for(const breakpoint of debugBreakpointBackups)
+    progWorker.postMessage({msgId: MSGID_DEBUG_ADD_BREAKPOINT, msgData: breakpoint});
+
+  if(isDebugging)
+    progWorker.postMessage({msgId: MSGID_DEBUG_ENABLE, msgData: null});
+
+  cleanupUI("Program stopped.");
+}
+
+function window_onLoad(event)
+//
+{
+  initUI();
+}
+
+function progUI_onMessage(message)
+//Process messages sent from the worker thread
+{
+  if(!isRunning)
+    return;
+
+  uiMessageMap.get(message.data.msgId)(message.data.msgData);
+}
+
+function toggle_onClick(event)
+{
+  this.parentElement.querySelector(".pane-open").classList.toggle("pane-closed");
+  this.classList.toggle("toggle-closed");
+}
+
+
+function onMsgProgDoneSuccess(msgData)
+//The worker thread has signaled that the program has completed successfully
+{
+  cleanupUI("Program run successfully.");
+}
+
+function onMsgProgDoneError(msgData)
+//The worker thread has signaled that the program has stopped with an error
+{
+  if(msgData.error.sourceName == mainSourceName)
+    selectEditorLine(msgData.error.sourceLineNum);
+
+  cleanupUI(msgData.error.message);
+}
+
+function onMsgStatusChange(msgData)
+//Display a status change
+{
+  statusBar.innerHTML = msgData.statusText;
+}
+
