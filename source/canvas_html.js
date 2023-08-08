e5fb2cf059cc4b258761b48850ee2ebeb8f7775d
CheesusChice <sinusoid314@gmail.com>
2023-08-08 19:40:18 -0400

    Delete source directory

--- source/canvas_html.js
+++
@@ -1,9 +0,0 @@
-const canvasHTML = 
-`
-<div id="canvasDiv">
-  <label id="canvasToggle" class="toggle-open">Canvas</label>
-  <div id="canvasPane" class="pane-open">
-    <canvas id="progCanvas" width=500 height=300 tabindex="0"></canvas>
-  </div>
-</div>
-`;
