7e37bb430f2aa3715412fa0a1079774c5f630a8b
CheesusChice <sinusoid314@gmail.com>
2024-12-30 10:11:33 -0500

    Add files via upload
    - Added skeleton file for Language Overview help section.
    - Added skeleton files for Code Editor help section.

--- docs/help/reference_doc.js
+++ docs/help/reference_doc.js
@@ -6,9 +6,9 @@ usageOpen.addEventListener("click", usageOpen_onClick);
 function usageOpen_onClick(event)
 //
 {
-  var fileText = usageCode.innerText;
+  var fileData = usageCode.innerText;
 
-  window.localStorage.setItem("fileText", fileText);
+  window.localStorage.setItem("fileData", fileData);
   window.open(event.target.href + "index.html?open=local", "_blank");
 
   event.preventDefault();
