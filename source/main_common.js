9c16e4e5c22cd3d3e61d28b9a126759e9c8ef5b4
Sinusoid314 <sinusoid314@gmail.com>
2026-07-22 23:19:26 -0400

    Started version 2.2.0.

--- source/main_common.js
+++ source/main_common.js
@@ -2,7 +2,7 @@ const MAIN_MODE_EDIT = 1;
 const MAIN_MODE_DEPLOY = 2;
 
 const mainSourceName = "<main>";
-const lbVersion = "2.1.0.36";
+const lbVersion = "2.2.0.0";
 
 //UI -> Worker messages
 const MSGID_START_PROG = 1;
